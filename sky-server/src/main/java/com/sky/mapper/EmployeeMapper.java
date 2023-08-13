d206269a4cd4dfa263baaf4d098124a9ef62af74
keptman <3158305205@qq.com>
2023-08-14 00:02:28 +0800

    完成编辑员工状态功能

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -35,4 +35,12 @@ public interface EmployeeMapper {
      * 根据主键修改状态
      */
     void update(Employee employee);
+
+    /**
+     * 根据id查询员工
+     * @param id
+     * @return
+     */
+    @Select("select * from employee where id = #{id}")
+    Employee getById(Long id);
 }
