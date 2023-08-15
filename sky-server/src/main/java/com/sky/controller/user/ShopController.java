09ab83f726929927fb45c1d7e442ac530c214b55
keptman <3158305205@qq.com>
2023-08-16 01:51:50 +0800

    店铺营业状态业务代码开发

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,33 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Api(tags = "店铺相关接口")
+@Slf4j
+public class ShopController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    public static final String KEY ="SHOP_STATUS";
+    /**
+     * 获取营业状态
+     *
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取营业状态")
+    public Result<Integer> getStatus() {
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("获取店铺营业状态: {}",status ==1? "营业中" : "打样中") ;
+        return Result.success(status);
+    }
+}
